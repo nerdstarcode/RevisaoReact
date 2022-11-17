2f433659c4468b57cdb1f86fba641f3db7c44128
sthiven <sthivendev@gmail.com>
2022-11-17 11:28:15 -0300

    :sparkles: Usando useLayoutEffect para requisição em api

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,7 @@
 import './style.css';
 
 import { Card } from '../../components/Card'
-import React, { useState, useEffect } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 
 
 
@@ -24,7 +24,7 @@ export function Home() {
     setNames(prevState => [...prevState, newName])//o prevState recupera valores anteriores do estado, pode-se usar o nome que quiser
   }
   // executa uma vez após a renderizão da página ou ao ter uma alteração em algo dentro do seu vetor de verificação
-  useEffect(()=>{
+  useLayoutEffect(()=>{
     fetch('https://api.github.com/users/nerdstarcode')
     .then(response => response.json())
     .then(data => {
